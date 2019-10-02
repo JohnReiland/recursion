15852e716c645f7ce3584269d9fa82fd87a44b33
JohnReiland <reiland.shanghai@gmail.com>
2019-10-02 00:00:30 -0400

    "Add object literal functionality for stringifyJSON"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,28 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  stringified = '';
+  if (Array.isArray(obj === true)) {
+
+  }
+
+  if ((typeof obj) === 'object') {
+    stringified += '{';
+    for (prop in obj) {
+      if (stringified !== '{') {
+        stringified += ',';
+      }
+      stringified += '\'';
+      stringified += prop;
+      stringified += '\':';
+      stringified += obj[prop];
+    }
+    stringified += '}';
+  }
+  return stringified;
 };
+
+
+stringifyJSON({ x: 5, y: 6 });                                            // expected output: "{'x':5,'y':6}"
+//stringifyJSON([new Number(3), new String('false'), new Boolean(false)]);  // "[3,'false',false]"
+//stringifyJSON({ x: [10, undefined, function(){}, Symbol('')] });          // "{'x':[10,null,null,null]}"
